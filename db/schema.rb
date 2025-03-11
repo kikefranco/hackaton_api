3a815e9b07521360f7a6bef912ac18a7ee39f093
jesusMava <jesusmacedo7@gmail.com>
2025-03-11 16:51:43 -0600

    feature: Modify gemfile change mysql for psql and add elastic Beanstalk

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_01_23_172340) do
-  create_table "users", charset: "utf8mb3", collation: "utf8mb3_uca1400_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "lastname"
     t.datetime "birthdate"
