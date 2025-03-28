bfb0bbfe41d4bd75de8bb2b5e5eb101d3aaa941e
kikefranco <luis.franco@wizeline.com>
2025-03-28 10:05:55 -0500

    the size of the image was limited up to 50 mb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,14 @@ class User < ApplicationRecord
   validates :password, presence: true
   validates :description, presence: true
   validates :image, presence: true
+  validate :image_size_validation
+
+  private
+
+  def image_size_validation
+    return unless image.present?
+    return unless image.size > 50.megabytes
+
+    errors.add(:image, 'should be less than 50MB')
+  end
 end
